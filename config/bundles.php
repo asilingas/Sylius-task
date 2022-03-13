ac6a4bae91cf7255750b3a46b425505e15c3cb9e
Artūras Šilingas <mayoctt@gmail.com>
2022-03-13 15:25:29 +0200

    Export file download

--- config/bundles.php
+++ config/bundles.php
@@ -59,4 +59,5 @@ return [
     SyliusLabs\DoctrineMigrationsExtraBundle\SyliusLabsDoctrineMigrationsExtraBundle::class => ['all' => true],
     Sylius\PayPalPlugin\SyliusPayPalPlugin::class => ['all' => true],
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
--- src/Controller/Admin/ExportController.php
+++ src/Controller/Admin/ExportController.php
@@ -2,26 +2,36 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Export\Export;
+use App\Repository\ExportRepository;
 use App\Service\ExportService;
 use App\Message\ExportRequest;
-use Sylius\Bundle\CoreBundle\Doctrine\ORM\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
  * @Route("/admin/export")
  */
 class ExportController extends AbstractController
 {
+    private ExportRepository $exportRepository;
+
     private ExportService $exportService;
 
     private MessageBusInterface $messageBus;
 
-    public function __construct(ExportService $exportService, MessageBusInterface $messageBus)
-    {
+    public function __construct(
+        ExportRepository $exportRepository,
+        ExportService $exportService,
+        MessageBusInterface $messageBus
+    ) {
+        $this->exportRepository = $exportRepository;
         $this->exportService = $exportService;
         $this->messageBus = $messageBus;
     }
@@ -33,7 +43,15 @@ class ExportController extends AbstractController
      */
     public function indexAction(Request $request, string $type): Response
     {
-        return $this->render('admin/export/index.html.twig', ['type' => $type]);
+        $exports = $this->exportRepository->getForUser($this->getUser());
+
+        return $this->render(
+            'admin/export/index.html.twig',
+            [
+                'type' => $type,
+                'exports' => $exports,
+            ]
+        );
     }
 
     /**
@@ -48,5 +66,22 @@ class ExportController extends AbstractController
         $this->addFlash('success', sprintf('Export file %s is being generated.', $export->getFilename()));
 
         return $this->redirectToRoute('custom_admin_export_index', ['type' => $type]);
+    }
+
+    /**
+     * Serves export file.
+     *
+     * @Route("/download/{guid}", name="custom_admin_export_download", methods={"GET"})
+     * @ParamConverter("export", class="App\Entity\Export\Export")
+     */
+    public function downloadAction(Export $export): Response
+    {
+        $response = new BinaryFileResponse('export/' . $export->getFilename());
+        $response->setContentDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            $export->getFilename()
+        );
+
+        return $response;
     }
 }
